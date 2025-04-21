3187169493c8e5e584bfd6d694b82dd166044847
xulei131401 <xulei13140107@126.com>
2025-04-21 19:48:45 +0800

    自动脚本 push

--- learn-collection/src/test/java/org/zz/collection/MapTests.java
+++ learn-collection/src/test/java/org/zz/collection/MapTests.java
@@ -2,19 +2,30 @@ package org.zz.collection;
 
 import java.util.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.zz.common.entity.DemoUser;
 
 public class MapTests {
+
     @Test
-    void runHashMap() {
+    void crud() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name", "许磊");
+        map.put("age", "27");
+        Assertions.assertEquals(2, map.size(), "创建一个map");
+        Assertions.assertEquals("许磊", map.get("name"), "读取map");
+        Assertions.assertEquals("许磊", map.remove("name"), "删除map的某一个key");
+        map.put("name", "张三");
+        Assertions.assertEquals("张三", map.get("name"), "修改map后再次读取");
+    }
+
+    @Test
+    void iter() {
         HashMap<String, Integer> map = new HashMap<>();
         map.put("apple", 1);
         map.put("banana", 2);
         map.put("cherry", 3);
-
-        System.out.println(map);
-        System.out.println(map);
 
         // 方式一：使用 entrySet 遍历键值对
         System.out.println("使用 entrySet 遍历：");
@@ -38,6 +49,40 @@ public class MapTests {
             Map.Entry<String, Integer> entry = iterator.next();
             System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
         }
+    }
+
+    @Test
+    void remove() {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("apple", 1);
+        map.put("banana", 2);
+        map.put("cherry", 3);
+
+        // for 循环中直接删除，报错
+        for (String key : map.keySet()) {
+            // java.util.ConcurrentModificationException
+            map.remove(key);
+            System.out.println("Key: " + key + ", Value: " + map.get(key));
+        }
+
+        // 使用iterator删除，正常
+        Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Integer> entry = iterator.next();
+            if (Objects.equals(entry.getKey(), "2")) {
+                iterator.remove();
+            }
+        }
+
+        // 使用entrySet.removeIf()删除，正常
+        map.entrySet().removeIf(entry -> Objects.equals(entry.getKey(), "2"));
+
+        // 通过value移除
+        map.values().removeIf(value -> value != 1);
+        // 通过key移除
+        map.keySet().removeIf(key -> !Objects.equals(key, "name"));
+        // 通过键/值的输入/组合删除
+        map.entrySet().removeIf(entry -> !Objects.equals(entry.getKey(), "asd"));
     }
 
 
@@ -64,110 +109,5 @@ public class MapTests {
 
         // 从map中读取的时候需要调用hashCode,put的时候不需要
         System.out.println(map.get("1"));
-    }
-
-
-    @Test
-    void runHashMapRemove() {
-        HashMap<String, Integer> map = new HashMap<>();
-        map.put("apple", 1);
-        map.put("banana", 2);
-        map.put("cherry", 3);
-
-        System.out.println(map);
-        System.out.println(map);
-
-        // 删除一
-        Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
-        while (iterator.hasNext()) {
-            Map.Entry<String, Integer> entry = iterator.next();
-            if (Objects.equals(entry.getKey(), "2")) {
-                iterator.remove();
-            }
-        }
-
-        // 删除二
-        //map.entrySet().removeIf(entry -> Objects.equals(entry.getKey(), "2"));
-
-        // 删除三
-        for (String key : map.keySet()) {
-            // java.util.ConcurrentModificationException
-            map.remove(key);
-            System.out.println("Key: " + key + ", Value: " + map.get(key));
-        }
-    }
-
-    public static void crud() {
-        HashMap<String, String> mp = new HashMap<>();
-        mp.put("name", "许磊");
-        mp.put("age", "27");
-        System.out.println(mp);
-
-        System.out.println(mp.get("name"));
-
-        String value = mp.remove("name");
-        System.out.println(value);
-        System.out.println(mp);
-    }
-
-    public static void iterator() {
-        HashMap<String, String> mp = new HashMap<>();
-        mp.put("name", "许磊");
-        mp.put("age", "27");
-
-        // 通过key进行遍历
-        Set<String> keySet = mp.keySet();
-        Iterator<String> it = keySet.iterator();
-        while (it.hasNext()) {                         //利用了Iterator迭代器**
-            String key = it.next();
-            String value = mp.get(key);
-            System.out.println(key + "=" + value);
-        }
-
-        // 推荐写法
-        for (String key : keySet) { // for : 迭代
-            String value = mp.get(key);
-            System.out.println(key + "=" + value);
-        }
-
-        // 通过entry进行遍历
-        Set<Map.Entry<String, String>> entrySet = mp.entrySet();
-        //遍历Set集合
-        Iterator<Map.Entry<String, String>> it1 = entrySet.iterator();
-        while (it1.hasNext()) {
-            //得到每一对对应关系
-            Map.Entry<String, String> entry = it1.next();
-            //通过每一对对应关系获取对应的key
-            String key = entry.getKey();
-            //通过每一对对应关系获取对应的value
-            String value = entry.getValue();
-            System.out.println(key + "=" + value);
-
-            // 删除元素需要使用迭代器的remove方法，而非mp的remove方法
-//            if(Objects.equals(entry.getKey(), "name")) {
-//                it1.remove();//使用迭代器的remove()方法删除元素
-//                 mp.remove(entry.getKey()); // 迭代器不允许使用mp自身的remove方法，ConcurrentModificationException
-//            }
-        }
-
-        // 推荐写法, 此写法不支持遍历过程中修改
-        for (Map.Entry<String, String> entry : entrySet) {
-            //通过每一对对应关系获取对应的key
-            String key = entry.getKey();
-            //通过每一对对应关系获取对应的value
-            String value = entry.getValue();
-            System.out.println(key + "=" + value);
-
-            mp.remove(key);
-        }
-
-        // 通过value移除
-        mp.values().removeIf(value -> !value.contains("1"));
-        // 通过key移除
-        mp.keySet().removeIf(key -> !Objects.equals(key, "name"));
-        // 通过键/值的输入/组合删除
-        mp.entrySet().removeIf(entry -> !Objects.equals(entry.getKey(), "asd"));
-
-        System.out.println(mp);
     }
 }
